ce1e359403e754b962432ea958b355dbe7af4b13
angelvenchev <angelvenchev@yahoo.com>
2015-08-30 14:22:52 +0300

    Get the project to build

--- SmartPlayer/Validators/ValidateMimeMultipartContentFilter.cs
+++ SmartPlayer/Validators/ValidateMimeMultipartContentFilter.cs
@@ -9,18 +9,18 @@ using System.Web.Http.Filters;
 
 namespace SmartPlayer.Validators
 {
-    public class ValidateMimeMultipartContentFilter
-    {
-        public override void OnActionExecuting(HttpActionContext actionContext)
-        {
-            //if (!actionContext.Request.Content.IsMimeMultipartContent())
-            //{
-            //    throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
-            //}
-        }
-        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
-        {
+    //public class ValidateMimeMultipartContentFilter
+    //{
+    //    public override void OnActionExecuting(HttpActionContext actionContext)
+    //    {
+    //        //if (!actionContext.Request.Content.IsMimeMultipartContent())
+    //        //{
+    //        //    throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+    //        //}
+    //    }
+    //    public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+    //    {
 
-        }
-    }
+    //    }
+    //}
 }
