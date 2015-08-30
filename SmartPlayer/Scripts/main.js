bbaaa604283b0cdf45c98b9cd0497a926d67a007
DianaKostova <dkostova92@yahoo.com>
2015-08-30 18:59:08 +0300

    Handle errors during uploading files

--- SmartPlayer/Scripts/main.js
+++ SmartPlayer/Scripts/main.js
@@ -2,9 +2,17 @@
     source: ["c++", "java", "php", "coldfusion", "javascript", "asp", "ruby", "python"]
 });
 
-function handleFiles(event) {
-    console.log("Files:", event);
-
+function handleFiles(files) {
+    var file = files[0];    
+    if (file.type != "audio/mp3") {
+        console.log("Invalid data type");
+        $('.uploadFiles .errorMessage').css('display', 'block');
+        $('.uploadFiles .errorMessage').text("The type of the file that you are trying to upload is invalid. Please select mp3 file!")
+        $('#ajaxUploadButton').prop('disabled', true);
+    } else {
+        $('.uploadFiles .errorMessage').css('display', 'none');
+        $('#ajaxUploadButton').prop('disabled', false);
+    }
 }
 
 function likeCurrentSong() {
