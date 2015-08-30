8cc438dc9a545308095638f3d18fb0b701edacda
angelvenchev <angelvenchev@yahoo.com>
2015-08-30 12:59:56 +0300

    Add front-end stuff Diana

--- SmartPlayer/App_Start/BundleConfig.cs
+++ SmartPlayer/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace SmartPlayer
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
+                        "~/Scripts/jquery-ui.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -22,6 +25,7 @@ namespace SmartPlayer
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/jquery-ui.css",
                       "~/Content/site.css"));
         }
     }
---
+++ SmartPlayer/Scripts/main.js
@@ -0,0 +1,46 @@
+﻿$("#autocomplete").autocomplete({
+    source: ["c++", "java", "php", "coldfusion", "javascript", "asp", "ruby", "python"]
+});
+
+
+function handleFiles(files) {
+    var file = files[0];
+    var reader = new FileReader();
+    reader.onload = onFileReadComplete;
+    reader.readAsText(file);
+}
+  
+function onFileReadComplete(event) { 
+    console.log("Event:", event);
+}
+
+
+
+function likeCurrentSong() {
+    console.log("LikeCurrentSong");
+    var currentSong = getCurrentSongName();
+    like(currentSong);
+}
+
+function dislikeCurrentSong() {
+    console.log("DisLikeCurrentSong");
+    var currentSong = getCurrentSongName();
+    dislike(currentSong);
+}
+
+function getCurrentSongName() {
+    return Song;
+}
+
+function like(song) {
+    console.log("Like");
+}
+
+function dislike(song) {
+    console.log("Dislike");
+}
+
+
+function togglePlayPause() {
+    console.log("TogglePlayPause");
+}
