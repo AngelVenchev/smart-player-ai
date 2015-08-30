a691b145ce678eef5fdf7ae00fe223cfcf32420e
angelvenchev <angelvenchev@yahoo.com>
2015-08-30 13:00:49 +0300

    Add file upload validator Diana

---
+++ SmartPlayer/Validators/ValidateMimeMultipartContentFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace SmartPlayer.Validators
+{
+    public class ValidateMimeMultipartContentFilter
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            //if (!actionContext.Request.Content.IsMimeMultipartContent())
+            //{
+            //    throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            //}
+        }
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+
+        }
+    }
+}
