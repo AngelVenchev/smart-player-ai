69c28554861a7cd082aa9555115d5514dc2ec5ed
DianaKostova <dkostova92@yahoo.com>
2016-01-09 12:32:04 +0200

    Change password requirements

--- SmartPlayer/App_Start/IdentityConfig.cs
+++ SmartPlayer/App_Start/IdentityConfig.cs
@@ -29,7 +29,6 @@ namespace SmartPlayer
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
