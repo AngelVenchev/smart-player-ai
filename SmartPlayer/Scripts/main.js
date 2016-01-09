31a75b9e17ce0b170ebe8cf9f4e168598a4e2497
DianaKostova <dkostova92@yahoo.com>
2016-01-09 13:00:57 +0200

    Add typeahead

--- SmartPlayer/Scripts/main.js
+++ SmartPlayer/Scripts/main.js
@@ -1,6 +1,46 @@
-﻿$("#autocomplete").autocomplete({
-    source: ["c++", "java", "php", "coldfusion", "javascript", "asp", "ruby", "python"]
+﻿// call GetAll
+$(document).ready(function () {
+    attachEventListeners();
 });
+
+var baseUrl = "http://localhost";
+localStorage.setItem("baseUrl", baseUrl);
+
+function attachEventListeners() {
+
+    var songs = getAllSongsNames();
+    $('.typeahead').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+    },
+    {
+        name: 'songs',
+        source: substringMatcher(songs)
+    });
+};
+
+var substringMatcher = function (strs) {
+    return function findMatches(q, cb) {
+        var matches, substringRegex;
+
+        // an array that will be populated with substring matches
+        matches = [];
+
+        // regex used to determine if a string contains the substring `q`
+        substrRegex = new RegExp(q, 'i');
+
+        // iterate through the pool of strings and for any string that
+        // contains the substring `q`, add it to the `matches` array
+        $.each(strs, function (i, str) {
+            if (substrRegex.test(str)) {
+                matches.push(str);
+            }
+        });
+
+        cb(matches);
+    };
+};
 
 function handleFiles(files) {
     var file = files[0];    
@@ -13,33 +53,4 @@ function handleFiles(files) {
         $('.uploadFiles .errorMessage').css('display', 'none');
         $('#ajaxUploadButton').prop('disabled', false);
     }
-}
-
-function likeCurrentSong() {
-    console.log("LikeCurrentSong");
-    var currentSong = getCurrentSongName();
-    like(currentSong);
-}
-
-function dislikeCurrentSong() {
-    console.log("DisLikeCurrentSong");
-    var currentSong = getCurrentSongName();
-    dislike(currentSong);
-}
-
-function getCurrentSongName() {
-    return Song;
-}
-
-function like(song) {
-    console.log("Like");
-}
-
-function dislike(song) {
-    console.log("Dislike");
-}
-
-
-function togglePlayPause() {
-    console.log("TogglePlayPause");
 }
