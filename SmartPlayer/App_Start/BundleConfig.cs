519da1469dc2ebabc2beff106c98fcd8418fdeda
DianaKostova <dkostova92@yahoo.com>
2016-01-09 12:25:46 +0200

    Typeahead

--- SmartPlayer/App_Start/BundleConfig.cs
+++ SmartPlayer/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace SmartPlayer
             bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
                         "~/Scripts/jquery-ui.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/typeahead").Include(
+                        "~/Scripts/typeahead.bundle.min.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
