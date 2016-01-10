bfa6c56d3ef2eef7e787c074195690c15306d6eb
angelvenchev <angelvenchev@yahoo.com>
2016-01-10 21:54:59 +0200

    EDMX Changes

--- SmartPlayer.Data/SmartPlayer.Designer.cs
+++ SmartPlayer.Data/SmartPlayer.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Projects\smart-player-ai\SmartPlayer.Data\SmartPlayer.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Projects\SmartPlayer\SmartPlayer.Data\SmartPlayer.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
